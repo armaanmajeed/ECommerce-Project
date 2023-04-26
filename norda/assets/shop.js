d3e7eaf52ca2baab40950fa8f7853636c54c98a7
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-04-26 09:38:53 +0500

    updated

---
+++ norda/assets/shop.js
@@ -0,0 +1,8 @@
+//Bismillah
+
+const heartIcon = document.querySelector('.Wishlist')
+console.log(heartIcon)
+document.addEventListener('click', (event) => {
+	event.preventDefault();
+	console.log(event.target)
+})
