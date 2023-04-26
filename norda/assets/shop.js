089d47e9042b26facebdac3eb3b9b86b9d6f0fcc
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-04-26 23:59:57 +0500

    pending...

--- norda/assets/shop.js
+++ norda/assets/shop.js
@@ -3,8 +3,9 @@ const heartIcon = document.querySelectorAll(".Wishlist");
 let img;
 let productName;
 let randomPrice;
-let text = [];
 document.addEventListener("click", addToWish);
+
+let text = [];
 
 function addToWish(event) {
   event.preventDefault();
@@ -14,20 +15,28 @@ function addToWish(event) {
       img = heartIcon[i].parentNode.parentNode.querySelector("img");
       productName = heartIcon[i].parentNode.parentNode.parentElement.querySelector("h3").textContent;
       randomPrice = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
-      text.push(`<tr><td class="product-thumbnail">
-                          <a href="#">${img}</a>
-                        </td>
-                        <td class="product-name"><a href="#">${productName}</a></td>
-                        <td class="product-price-cart"><span class="amount">$${randomPrice}</span></td>
-                        <td class="product-quantity pro-details-quality">
-                          <div class="cart-plus-minus">
-                            <input class="cart-plus-minus-box" type="text" name="qtybutton" value="4" />
-                            </div>
-                            </td>
-                            <td class="product-subtotal">$${randomPrice - 50}</td>
-                            <td class="product-wishlist-cart">
-                            <a href="#">add to cart</a>
-                            </td></tr>`);
+      text.push(`<tr>
+      <td class="product-thumbnail">
+        <a href="#">${img}</a>
+      </td>
+      <td class="product-name">
+        <a href="#">${productName}</a>
+      </td>
+      <td class="product-price-cart">
+        <span class="amount">$${randomPrice}</span>
+      </td>
+      <td class="product-quantity pro-details-quality">
+        <div class="cart-plus-minus">
+          <input class="cart-plus-minus-box" type="text" name="qtybutton" value="4" />
+        </div>
+      </td>
+      <td class="product-subtotal">$${randomPrice - 50}</td>
+      <td class="product-wishlist-cart">
+        <a href="#"> add to cart </a>
+      </td>
+    </tr>`);
+
+
       localStorage.setItem("Wishes", text);
     }
   }
