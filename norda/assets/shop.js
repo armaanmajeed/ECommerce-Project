3d83926729503b9e7d9197055068dd5660db572b
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-04-26 19:27:32 +0500

    updated

--- norda/assets/shop.js
+++ norda/assets/shop.js
@@ -1,40 +1,36 @@
 //Bismillah
+const heartIcon = document.querySelectorAll(".Wishlist");
+let img;
+let productName;
+let randomPrice;
+let text;
+document.addEventListener("click", addToWish);
 
-const heartIcon = document.querySelectorAll('.Wishlist')
-export const tableBody = document.getElementById('wishTable')
+function addToWish(event) {
+  event.preventDefault();
+  const target = event.target;
+  for (let i = 0; i < heartIcon.length; i++) {
+    if (target == heartIcon[i]) {
+      img = heartIcon[i].parentNode.parentNode.querySelector("img");
+      productName = heartIcon[i].parentNode.parentNode.parentElement.querySelector("h3").textContent;
+      randomPrice = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
+      text = `<tr><td class="product-thumbnail">
+                          <a href="#">${img}</a>
+                        </td>
+                        <td class="product-name"><a href="#">${productName}</a></td>
+                        <td class="product-price-cart"><span class="amount">$${randomPrice}</span></td>
+                        <td class="product-quantity pro-details-quality">
+                          <div class="cart-plus-minus">
+                            <input class="cart-plus-minus-box" type="text" name="qtybutton" value="4" />
+                            </div>
+                            </td>
+                            <td class="product-subtotal">$${randomPrice - 50}</td>
+                            <td class="product-wishlist-cart">
+                            <a href="#">add to cart</a>
+                            </td></tr>`;
 
-export let img;
-export let productName;
-export let randomPrice;
+      window.localStorage.setItem("Wishes", text);
 
-document.addEventListener('click', addToWish);
-export function addToWish(event) {
-    event.preventDefault();
-    const target = event.target;
-    for (let i = 0; i < heartIcon.length; i++) {
-        if (target == heartIcon[i]) {
-            img = heartIcon[i].parentNode.parentNode.querySelector('img');
-            productName = heartIcon[i].parentNode.parentNode.parentElement.querySelector('h3').textContent;
-            randomPrice = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
-            console.log(img, randomPrice)
-
-        }
     }
+  }
 }
-
-// < tr >
-//                     <td class="product-thumbnail">
-//                       <a href="#"><img src="assets/images/cart/cart-1.jpg" alt="" /></a>
-//                     </td>
-//                     <td class="product-name"><a href="#">Simple Black T-Shirt</a></td>
-//                     <td class="product-price-cart"><span class="amount">$260.00</span></td>
-//                     <td class="product-quantity pro-details-quality">
-//                       <div class="cart-plus-minus">
-//                         <input class="cart-plus-minus-box" type="text" name="qtybutton" value="1" />
-//                       </div>
-//                     </td>
-//                     <td class="product-subtotal">$110.00</td>
-//                     <td class="product-wishlist-cart">
-//                       <a href="#">add to cart</a>
-//                     </td>
-//                   </ >
